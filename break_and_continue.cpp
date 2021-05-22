1b3c1443351719c76c832ec9b05282ecf5993c1e
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-22 09:36:46 +0530

    Break and Continue file uploaded

---
+++ break_and_continue.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /* code */
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
+        cout<<i<<endl;
+        if (i==2){
+        /* code */
+         break;
+
+        }
+            
+        
+    }
+    
+    return 0;
+}
+
